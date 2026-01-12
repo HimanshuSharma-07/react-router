fb931a02445d7045c2cfd24291961f646277c34b
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-01-12 22:44:43 +0530

    add About component with introductory content

---
+++ src/pages/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+
+function About() {
+    return (
+        <div className="py-16 bg-white">
+            <div className="container m-auto px-6 text-gray-600 md:px-12 xl:px-6">
+                <div className="space-y-6 md:space-y-0 md:flex md:gap-6 lg:items-center lg:gap-12">
+                    <div className="md:5/12 lg:w-5/12">
+                        <img
+                            src="https://imgs.search.brave.com/_iw4i-p9m2Lt1bnFAjCcXJmMbHq6dtEQ9Tz_YrGk8aw/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvMTUvUmVh/Y3QtTG9nby1QTkct/SW1hZ2VzLnBuZw"
+                            alt="image"
+                        />
+                    </div>
+                    <div className="md:7/12 lg:w-6/12">
+                        <h2 className="text-2xl text-gray-900 font-bold md:text-4xl">
+                            React development is carried out by passionate developers
+                        </h2>
+                        <p className="mt-6 text-gray-600">
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eum omnis voluptatem
+                            accusantium nemo perspiciatis delectus atque autem! Voluptatum tenetur beatae unde
+                            aperiam, repellat expedita consequatur! Officiis id consequatur atque doloremque!
+                        </p>
+                        <p className="mt-4 text-gray-600">
+                            Nobis minus voluptatibus pariatur dignissimos libero quaerat iure expedita at?
+                            Asperiores nemo possimus nesciunt dicta veniam aspernatur quam mollitia.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default About
