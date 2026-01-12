a5ed839cbed49e14834cbc1bd7c498b41e80d1ae
Himanshu Sharma <sharmahimanshu82194@gmail.com>
2026-01-12 22:46:00 +0530

    refactor App component to implement routing for Home, About, and Contact pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
+import Home from './pages/Home'
+import { Route, Routes } from 'react-router-dom'
+import About from './pages/About'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import Contact from './pages/Contact'
 
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+    <NavBar />
+
+      <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/contact' element={<Contact />} />
+      </Routes>
+
+    <Footer />
+    </>
   )
 }
 
